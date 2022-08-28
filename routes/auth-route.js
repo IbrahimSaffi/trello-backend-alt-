a9d8f35dcb297a336e720d5bd0294779bec7234d
Fahmid Sakib <fahmidsakib97@gmail.com>
2022-08-28 17:42:47 +0600

    Updated

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -8,12 +8,12 @@ router.post('/signup', async (req, res) => {
     const { email, name, password, confirmPassword } = req.body
     if (!email || !name || !password || !confirmPassword) return res.status(400).json({ error: 'All fields are required' })
     if (password !== confirmPassword) return res.status(400).json({ error: 'Password and confirm password do not match' })
-    const existingUser = await userModel.findOne({ email: email })
+    const existingUser = await userModel.findOne({ email: email.toLowerCase() })
     if (existingUser !== null) return res.status(400).json({ error: 'Email already exists' })
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
-    const newUesr = new userModel({ name, email, password: hash, })
-    const frontendUser = { name, email }
+    const newUesr = new userModel({ name, email: email.toLowerCase(), password: hash, })
+    const frontendUser = { name, email: email.toLowerCase() }
     try {
         const savedUser = await newUesr.save()
         frontendUser._id = savedUser._id
@@ -26,11 +26,11 @@ router.post('/signup', async (req, res) => {
 router.post('/signin', async (req, res) => {
     const { email, password } = req.body
     if (!email || !password) return res.status(400).json({ error: 'Please provide email and password' })
-    const existingUser = await userModel.findOne({ email: email.toLowarCase() })
+    const existingUser = await userModel.findOne({ email: email.toLowerCase() })
     if (existingUser === null) return res.status(400).json({ error: 'User not found' })
     const result = bcrypt.compareSync(password, existingUser.password)
     if (result) {
-        const payload = { _id: existingUser._id, name: existingUser.name, email: email.toLowarCase() }
+        const payload = { _id: existingUser._id, name: existingUser.name, email: email.toLowerCase() }
         const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRATION_TIME })
         const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: process.env.REFRESH_TOKEN_EXPIRATION_TIME })
         return res.status(200).json({ data: { refreshToken, accessToken, payload } })
